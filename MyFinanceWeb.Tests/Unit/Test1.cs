6023da7f9f24736fb96161a949fb342451879ba4
Rodrigo Pires Meira <rpmeir@gmail.com>
2026-09-13 15:44:10 -0300

    Add MSTest project and initial test class for unit testing

---
+++ MyFinanceWeb.Tests/Unit/Test1.cs
@@ -0,0 +1,13 @@
+namespace MyFinanceWeb.Tests.Unit;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.IsTrue(true);
+    }
+}
